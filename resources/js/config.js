d0e55671b86b86b3646cda9b92b0a9002fa93ec3
Luis Angel <langelespin@gmail.com>
2024-11-15 17:31:18 -0600

    merge cambios, config file

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,8 +1,8 @@
 //var server="http://148.215.24.32:8080";
 //var cargarJson="../aura/resources/js/json/";
 
-//var server="http://148.215.2.20:8080";
-server = "http://148.215.24.119:8080";
+//var server="http://148.215.24.37:8080";
+//server = "http://148.215.24.119:8080";
 
-//var server="https://portal.amelica.org";
-var cargarJson="../Aura/resources/js/json/";
+var server="https://portal.amelica.org";
+var cargarJson="../resources/js/json/";
